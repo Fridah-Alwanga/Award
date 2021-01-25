f98b6bce33f9e9e14b7aaba034497587bd0340f1
Fridah-Alwanga <fridahalwanga6@gmail.com>
2021-01-25 10:24:18 +0300

    modify settings files

--- myaward/settings.py
+++ myaward/settings.py
@@ -44,6 +44,12 @@ INSTALLED_APPS = [
     'bootstrap4'
 ]
 
+
+UPLOADCARE = {
+    'pub_key': '2b709bca64245dd9e55e',
+    'secret': '0a60851de5f3db2dc728',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
