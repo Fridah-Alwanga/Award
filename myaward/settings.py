948ea512c10fc34b186d95e3dd5b7b8360bdd33f
Fridah-Alwanga <fridahalwanga6@gmail.com>
2021-01-25 08:43:37 +0300

    update files

--- myaward/settings.py
+++ myaward/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'award',
+    'pyuploadcare.dj',
+    'rest_framework',
+    'crispy_forms',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,14 @@ WSGI_APPLICATION = 'myaward.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myaward',
+        'USER': 'postgres',
+        'PASSWORD':'Access',
+        'HOST':'localhost',
+        'PORT': '5432',
+
+        
     }
 }
 
