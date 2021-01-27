bc67421f54859430e4c43355705ed3662b67e5ca
Fridah-Alwanga <fridahalwanga6@gmail.com>
2021-01-27 12:40:27 +0300

    deploy to heroku

--- myaward/settings.py
+++ myaward/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+SECRET_KEY = 'p8dxp14*%gjgqak8-4)+)ha%s_(c84p8q5d3yk0hivu)l0s964'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
